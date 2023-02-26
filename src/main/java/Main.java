7bea70309a3b2e21a2956fd654fb7915070c1bc8
JIWEON-JEONG <dlektl6@naver.com>
2023-02-26 16:09:50 +0900

    remove : 사용하지 않는 행위 삭제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import di.ApplicationContext;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		IntroduceService s = new IntroduceService();
 		IntroduceService introduceService = ApplicationContext.getInstance(IntroduceService.class);
 		introduceService.introduce();
 		introduceService.coding();
